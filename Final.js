2020b0dba7bfb9200661e8ddbab8b5201c688210
crazyme03 <messier04.em@gmail.com>
2020-02-05 13:12:57 -0500

    Made winners win

--- Final.js
+++ Final.js
@@ -6,12 +6,18 @@ function tic(x) {
     ticTacBoard[x] = 1
     makeCross(x);
   }
+  if ((ticTacBoard[0] == 1 && ticTacBoard[1] == 1 && ticTacBoard[2] == 1) || (ticTacBoard[3] == 1 && ticTacBoard[4] == 1 && ticTacBoard[5] == 1) || (ticTacBoard[6] == 1 && ticTacBoard[7] == 1 && ticTacBoard[8] == 1) || (ticTacBoard[0] == 1 && ticTacBoard[3] == 1 && ticTacBoard[6] == 1) || (ticTacBoard[1] == 1 && ticTacBoard[4] == 1 && ticTacBoard[7] == 1) || (ticTacBoard[2] == 1 && ticTacBoard[5] == 1 && ticTacBoard[8] == 1) || (ticTacBoard[0] == 1 && ticTacBoard[4] == 1 && ticTacBoard[8] == 1) || (ticTacBoard[2] == 1 && ticTacBoard[4] == 1 && ticTacBoard[6] == 1)) {
+    document.getElementById('ticTacBody').innerHTML += "<p>Cross Won!</p>";
+  }
 }
 
 function tac(x) {
   if (ticTacBoard[x] == 0) {
     ticTacBoard[x] = 2
     makeCirc(x);
+  }
+  if ((ticTacBoard[0] == 2 && ticTacBoard[1] == 2 && ticTacBoard[2] == 2) || (ticTacBoard[3] == 2 && ticTacBoard[4] == 2 && ticTacBoard[5] == 2) || (ticTacBoard[6] == 2 && ticTacBoard[7] == 2 && ticTacBoard[8] == 2) || (ticTacBoard[0] == 2 && ticTacBoard[3] == 2 && ticTacBoard[6] == 2) || (ticTacBoard[1] == 2 && ticTacBoard[4] == 2 && ticTacBoard[7] == 2) || (ticTacBoard[2] == 2 && ticTacBoard[5] == 2 && ticTacBoard[8] == 2) || (ticTacBoard[0] == 2 && ticTacBoard[4] == 2 && ticTacBoard[8] == 2) || (ticTacBoard[2] == 2 && ticTacBoard[4] == 2 && ticTacBoard[6] == 2)) {
+    document.getElementById('ticTacBody').innerHTML += "<p>Circles Won!</p>";
   }
 }
 
