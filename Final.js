faa848f3420f03a27f381f45b3f2e60fd9f05cd9
crazyme03 <messier04.em@gmail.com>
2020-02-05 13:37:14 -0500

    Added a reset to the board

--- Final.js
+++ Final.js
@@ -1,13 +1,14 @@
-let ticTacBoard = [0,0,0,0,0,0,0,0,0];
-let toer = 0
-
+var ticTacBoard = [0,0,0,0,0,0,0,0,0];
+var toer = 0;
+var over = false;
 function tic(x) {
   if (ticTacBoard[x] == 0) {
     ticTacBoard[x] = 1
     makeCross(x);
   }
   if ((ticTacBoard[0] == 1 && ticTacBoard[1] == 1 && ticTacBoard[2] == 1) || (ticTacBoard[3] == 1 && ticTacBoard[4] == 1 && ticTacBoard[5] == 1) || (ticTacBoard[6] == 1 && ticTacBoard[7] == 1 && ticTacBoard[8] == 1) || (ticTacBoard[0] == 1 && ticTacBoard[3] == 1 && ticTacBoard[6] == 1) || (ticTacBoard[1] == 1 && ticTacBoard[4] == 1 && ticTacBoard[7] == 1) || (ticTacBoard[2] == 1 && ticTacBoard[5] == 1 && ticTacBoard[8] == 1) || (ticTacBoard[0] == 1 && ticTacBoard[4] == 1 && ticTacBoard[8] == 1) || (ticTacBoard[2] == 1 && ticTacBoard[4] == 1 && ticTacBoard[6] == 1)) {
-    document.getElementById('ticTacBody').innerHTML += "<p>Cross Won!</p>";
+    document.getElementById('ticTacBody').innerHTML += "<p id='winner'>Cross Won!</p>";
+    over = true;
   }
 }
 
@@ -17,11 +18,26 @@ function tac(x) {
     makeCirc(x);
   }
   if ((ticTacBoard[0] == 2 && ticTacBoard[1] == 2 && ticTacBoard[2] == 2) || (ticTacBoard[3] == 2 && ticTacBoard[4] == 2 && ticTacBoard[5] == 2) || (ticTacBoard[6] == 2 && ticTacBoard[7] == 2 && ticTacBoard[8] == 2) || (ticTacBoard[0] == 2 && ticTacBoard[3] == 2 && ticTacBoard[6] == 2) || (ticTacBoard[1] == 2 && ticTacBoard[4] == 2 && ticTacBoard[7] == 2) || (ticTacBoard[2] == 2 && ticTacBoard[5] == 2 && ticTacBoard[8] == 2) || (ticTacBoard[0] == 2 && ticTacBoard[4] == 2 && ticTacBoard[8] == 2) || (ticTacBoard[2] == 2 && ticTacBoard[4] == 2 && ticTacBoard[6] == 2)) {
-    document.getElementById('ticTacBody').innerHTML += "<p>Circles Won!</p>";
+    document.getElementById('ticTacBody').innerHTML += "<p id='winner'>Circles Won!</p>";
+    over = true;
   }
 }
 
+function resetBoard() {
+  for (i = 0; i < 9; i++) {
+    ticTacBoard[i] = 0;
+    document.getElementById('a'+i).innerHTML = '';
+  }
+}
 function toe(x) {
+  if (toer == 9 || over == true) {
+    resetBoard();
+    if (toer == 9 && over == false){
+      document.getElementById('ticTacBody').innerHTML += "<p id='winner'>Draw!</p>";
+    }
+    toer = 0;
+    over = false;
+  }
   if (toer%2 == 0) {
     tic(x);
   } else {
